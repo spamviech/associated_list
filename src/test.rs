c7a4647e1d3ca65f87987156bc057ae8e9078571
spamviech <spamviech@web.de>
2024-01-24 21:53:59 +0100

    add simplistic TestAllocator for testing new_in and with_capacity_in

--- src/test.rs
+++ src/test.rs
@@ -3,6 +3,16 @@
 use alloc::collections::BTreeMap;
 
 use crate::AssocList;
+#[cfg(feature = "allocator_api")]
+use alloc::sync::Arc;
+#[cfg(feature = "allocator_api")]
+use core::{
+    alloc::{AllocError, Allocator, Layout},
+    cell::Cell,
+    pin::Pin,
+    ptr::NonNull,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
 // O(n*log(n))
 fn unique_ord_keys<K: Ord, V>(assoc_list: AssocList<K, V>) -> bool {
@@ -20,25 +30,82 @@ fn new() {
 
 #[test]
 fn default() {
-    let assoc_list: AssocList<usize, f64> = AssocList::default();
+    let assoc_list: AssocList<i32, &'static str> = AssocList::default();
     assert!(assoc_list.vec.is_empty());
     assert!(unique_ord_keys(assoc_list));
 }
 
 #[test]
 fn with_capacity() {
-    const CAPACITY: usize = 23571;
-    let assoc_list: AssocList<usize, f64> = AssocList::with_capacity(CAPACITY);
+    const CAPACITY: usize = 23_571;
+    let assoc_list: AssocList<u16, f32> = AssocList::with_capacity(CAPACITY);
     assert!(assoc_list.vec.is_empty());
     assert_eq!(assoc_list.vec.capacity(), CAPACITY);
     assert!(unique_ord_keys(assoc_list));
 }
 
+#[cfg(feature = "allocator_api")]
+/// Simplistic [`Allocator`] for using in the test.
+///
+/// Once acquired, memory will never be freed again.
+struct TestAllocator<const N: usize> {
+    /// Control access to the other fields.
+    current: AtomicBool,
+    /// The available memory.
+    memory: Pin<Arc<[u8; N]>>,
+    /// The next free memory location.
+    next: Cell<usize>,
+}
+
+#[cfg(feature = "allocator_api")]
+impl<const N: usize> TestAllocator<N> {
+    /// Create a new [`TestAllocator`].
+    #[must_use]
+    fn new() -> Self {
+        TestAllocator { used: AtomicBool::new(false), memory: Arc::pin([0; N]), next: Cell::new(0) }
+    }
+}
+
+#[cfg(feature = "allocator_api")]
+impl<const N: usize> Allocator for TestAllocator<N> {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        // helper-function, assuming we have access. This allows using the `?`-operator.
+        fn allocate_impl(allocator: &Self, layout: Layout) -> Option<NonNull<[u8]>> {
+            // make sure the returned memory is aligned correctly
+            let next_rounded_up = allocator.next.get().checked_next_multiple_of(align)?;
+            // make sure the layout fits into the remaining memory
+            let size = layout.size();
+            let last_bit = next_rounded_up.checked_add(size)?;
+            if (last_bit > N) {
+                return None;
+            }
+            let slice = &allocator.memory[next_rounded_up..=last_bit];
+            let non_null = NonNull::new(slice.as_mut_ptr())?;
+            self.next.set(last_bit.saturating_add(1));
+            Some(non_null)
+        }
+        // spin-loop, wait for access
+        while (!self.used.swap(true, Ordering::AcqRel)) {}
+        // allocate the memory
+        let ret = allocate_impl(self, layout);
+        // free usage again
+        self.used.store(false, Ordering::Release);
+        // return result
+        ret.ok_or(AllocError)
+    }
+
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        // simplistic, we don't free any memory
+    }
+}
+
+#[cfg(feature = "allocator_api")]
 #[test]
 fn new_in() {
     todo!()
 }
 
+#[cfg(feature = "allocator_api")]
 #[test]
 fn with_capacity_in() {
     todo!()
