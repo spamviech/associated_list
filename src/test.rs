a4920a36fe4d35d12229c99cc95c67b7ce19d10e
spamviech <spamviech@web.de>
2024-01-26 22:59:39 +0100

    finalize implementation for allocator-based tests

--- src/test.rs
+++ src/test.rs
@@ -2,20 +2,19 @@
 
 use alloc::collections::BTreeMap;
 
-use crate::AssocList;
-#[cfg(feature = "allocator_api")]
-use alloc::sync::Arc;
 #[cfg(feature = "allocator_api")]
 use core::{
-    alloc::{AllocError, Allocator, Layout},
-    cell::Cell,
-    pin::Pin,
+    alloc::{AllocError, Allocator as ActualAllocator, Layout},
+    cell::{Cell, UnsafeCell},
+    pin::{pin, Pin},
     ptr::NonNull,
     sync::atomic::{AtomicBool, Ordering},
 };
 
+use crate::{Allocator, AssocList};
+
 // O(n*log(n))
-fn unique_ord_keys<K: Ord, V>(assoc_list: AssocList<K, V>) -> bool {
+fn unique_ord_keys<K: Ord, V, A: Allocator>(assoc_list: AssocList<K, V, A>) -> bool {
     let size = assoc_list.len();
     let b_tree_map: BTreeMap<_, _> = assoc_list.vec.into_iter().collect();
     size == b_tree_map.len()
@@ -48,44 +47,56 @@ fn with_capacity() {
 /// Simplistic [`Allocator`] for using in the test.
 ///
 /// Once acquired, memory will never be freed again.
-struct TestAllocator<const N: usize> {
+struct TestAllocator<'t> {
     /// Control access to the other fields.
-    current: AtomicBool,
+    used: AtomicBool,
     /// The available memory.
-    memory: Pin<Arc<[u8; N]>>,
+    memory: UnsafeCell<Pin<&'t mut [u8]>>,
     /// The next free memory location.
     next: Cell<usize>,
 }
 
 #[cfg(feature = "allocator_api")]
-impl<const N: usize> TestAllocator<N> {
-    /// Create a new [`TestAllocator`].
+impl<'t> TestAllocator<'t> {
+    /// Create a new [`TestAllocator`] using the provided `memory`.
+    ///
+    /// It can be created using the [`pin!`](core::pin::pin)-macro.
     #[must_use]
-    fn new() -> Self {
-        TestAllocator { used: AtomicBool::new(false), memory: Arc::pin([0; N]), next: Cell::new(0) }
+    fn new(memory: Pin<&'t mut [u8]>) -> Self {
+        TestAllocator {
+            used: AtomicBool::new(false),
+            memory: UnsafeCell::new(memory),
+            next: Cell::new(0),
+        }
     }
 }
 
 #[cfg(feature = "allocator_api")]
-impl<const N: usize> Allocator for TestAllocator<N> {
+#[allow(unsafe_code)]
+unsafe impl ActualAllocator for TestAllocator<'_> {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
         // helper-function, assuming we have access. This allows using the `?`-operator.
-        fn allocate_impl(allocator: &Self, layout: Layout) -> Option<NonNull<[u8]>> {
+        fn allocate_impl(allocator: &TestAllocator<'_>, layout: Layout) -> Option<NonNull<[u8]>> {
             // make sure the returned memory is aligned correctly
-            let next_rounded_up = allocator.next.get().checked_next_multiple_of(align)?;
+            let next_rounded_up = allocator.next.get().checked_next_multiple_of(layout.align())?;
             // make sure the layout fits into the remaining memory
             let size = layout.size();
             let last_bit = next_rounded_up.checked_add(size)?;
-            if (last_bit > N) {
+
+            #[allow(unsafe_code)]
+            // SAFETY: protected by spin-looping on `self.used` before calling this function
+            if last_bit > unsafe { &*allocator.memory.get() }.len() {
                 return None;
             }
-            let slice = &allocator.memory[next_rounded_up..=last_bit];
-            let non_null = NonNull::new(slice.as_mut_ptr())?;
-            self.next.set(last_bit.saturating_add(1));
+            #[allow(unsafe_code)]
+            // SAFETY: protected by spin-looping on `self.used` before calling this function
+            let slice: &mut [u8] = unsafe { &mut *allocator.memory.get() };
+            let non_null = NonNull::new(slice)?;
+            allocator.next.set(last_bit.saturating_add(1));
             Some(non_null)
         }
         // spin-loop, wait for access
-        while (!self.used.swap(true, Ordering::AcqRel)) {}
+        while !self.used.swap(true, Ordering::AcqRel) {}
         // allocate the memory
         let ret = allocate_impl(self, layout);
         // free usage again
@@ -94,7 +105,8 @@ impl<const N: usize> Allocator for TestAllocator<N> {
         ret.ok_or(AllocError)
     }
 
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+    #[allow(unsafe_code)]
+    unsafe fn deallocate(&self, _ptr: NonNull<u8>, _layout: Layout) {
         // simplistic, we don't free any memory
     }
 }
@@ -102,13 +114,23 @@ impl<const N: usize> Allocator for TestAllocator<N> {
 #[cfg(feature = "allocator_api")]
 #[test]
 fn new_in() {
-    todo!()
+    let memory = pin!([0; 0]);
+    let test_allocator = TestAllocator::new(memory);
+    let assoc_list: AssocList<usize, f64, _> = AssocList::new_in(test_allocator);
+    assert!(assoc_list.vec.is_empty());
+    assert!(unique_ord_keys(assoc_list));
 }
 
 #[cfg(feature = "allocator_api")]
 #[test]
 fn with_capacity_in() {
-    todo!()
+    let memory = pin!([0; 24]);
+    let test_allocator = TestAllocator::new(memory);
+    const CAPACITY: usize = 3;
+    let assoc_list: AssocList<u16, f32, _> = AssocList::with_capacity_in(CAPACITY, test_allocator);
+    assert!(assoc_list.vec.is_empty());
+    assert_eq!(assoc_list.vec.capacity(), CAPACITY);
+    assert!(unique_ord_keys(assoc_list));
 }
 
 #[test]
