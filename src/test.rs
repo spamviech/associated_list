cba42c04bb9a89405934baf55e1bbd8b944e33c5
spamviech <spamviech@web.de>
2024-01-27 18:30:28 +0100

    quickcheck-tests for the remaining iterator-methods

--- src/test.rs
+++ src/test.rs
@@ -25,6 +25,11 @@ fn unique_ord_keys<K: Ord, V, A: Allocator>(assoc_list: AssocList<K, V, A>) -> b
     size == b_tree_map.len()
 }
 
+// Move the reference from the tuple to the elements
+fn split_tuple_refs<A, B>((key, value): &(A, B)) -> (&A, &B) {
+    (key, value)
+}
+
 #[test]
 fn new() {
     const ASSOC_LIST: AssocList<usize, f64> = AssocList::new();
@@ -309,9 +314,7 @@ fn into_values() {
 fn iter(input: Vec<(u64, i32)>) {
     let assoc_list: AssocList<_, _> = input.iter().copied().collect();
     let reference_map: BTreeMap<_, _> = input.into_iter().collect();
-    // convert from &(K,V) to (&K, &V)
-    #[allow(clippy::map_identity)]
-    let mut actual_values: Vec<_> = assoc_list.iter().map(|(key, value)| (key, value)).collect();
+    let mut actual_values: Vec<_> = assoc_list.iter().map(split_tuple_refs).collect();
     let mut expected_values: Vec<_> = reference_map.iter().collect();
 
     actual_values.sort_by_key(|(key, _value)| *key);
@@ -319,29 +322,75 @@ fn iter(input: Vec<(u64, i32)>) {
     assert_eq!(actual_values, expected_values);
 }
 
-#[test]
-fn iter_mut() {
-    todo!()
+#[quickcheck]
+fn iter_mut(input: Vec<(i64, i32)>) {
+    let mut assoc_list: AssocList<_, _> = input.iter().copied().collect();
+    let mut reference_map: BTreeMap<_, _> = input.into_iter().collect();
+    let mut actual_values: Vec<_> = assoc_list.iter_mut().collect();
+    let mut expected_values: Vec<_> = reference_map.iter_mut().collect();
+
+    actual_values.sort_by_key(|(key, _value)| *key);
+    expected_values.sort_by_key(|(key, _value)| *key);
+    assert_eq!(actual_values, expected_values, "iter_mut returns the correct elements");
+
+    let new_value = 1267;
+    assoc_list.values_mut().for_each(|value| *value = new_value);
+    assert!(
+        assoc_list.values().all(|value| *value == new_value),
+        "iter_mut elements can influence the values in the AssocList"
+    );
 }
 
-#[test]
-fn drain() {
-    todo!()
+#[quickcheck]
+fn drain(input: Vec<(i16, u8)>) {
+    let mut assoc_list: AssocList<_, _> = input.iter().copied().collect();
+    let reference_map: BTreeMap<_, _> = input.into_iter().collect();
+    let actual_values: BTreeMap<_, _> = assoc_list.drain().collect();
+
+    assert_eq!(actual_values, reference_map, "drain returns the correct elements");
+
+    assert!(assoc_list.is_empty(), "drain removes all elements from the AssocList");
 }
 
-#[test]
-fn into_iter() {
-    todo!()
+#[quickcheck]
+fn into_iter(input: Vec<(u8, i32)>) {
+    let assoc_list: AssocList<_, _> = input.iter().copied().collect();
+    let reference_map: BTreeMap<_, _> = input.into_iter().collect();
+    let actual_values: BTreeMap<_, _> = assoc_list.into_iter().collect();
+
+    assert_eq!(actual_values, reference_map);
 }
 
-#[test]
-fn into_iter_ref() {
-    todo!()
+// required by quickcheck
+#[allow(clippy::needless_pass_by_value)]
+#[quickcheck]
+fn into_iter_ref(input: Vec<(i64, i16)>) {
+    let assoc_list: AssocList<_, _> = input.iter().copied().collect();
+    #[allow(clippy::into_iter_on_ref)]
+    let reference_map: BTreeMap<_, _> = (&input).into_iter().map(split_tuple_refs).collect();
+    let actual_values: BTreeMap<_, _> = (&assoc_list).into_iter().map(split_tuple_refs).collect();
+
+    assert_eq!(actual_values, reference_map);
 }
 
-#[test]
-fn into_iter_mut() {
-    todo!()
+#[quickcheck]
+fn into_iter_mut(input: Vec<(i64, u32)>) {
+    let mut assoc_list: AssocList<_, _> = input.iter().copied().collect();
+    let mut reference_map: BTreeMap<_, _> = input.into_iter().collect();
+    let mut actual_values: Vec<_> = (&mut assoc_list).into_iter().collect();
+    #[allow(clippy::into_iter_on_ref)]
+    let mut expected_values: Vec<_> = (&mut reference_map).into_iter().collect();
+
+    actual_values.sort_by_key(|(key, _value)| *key);
+    expected_values.sort_by_key(|(key, _value)| *key);
+    assert_eq!(actual_values, expected_values, "&mut into_iter returns the correct elements");
+
+    let new_value = 2347;
+    assoc_list.values_mut().for_each(|value| *value = new_value);
+    assert!(
+        assoc_list.values().all(|value| *value == new_value),
+        "&mut into_iter elements can influence the values in the AssocList"
+    );
 }
 
 #[test]
