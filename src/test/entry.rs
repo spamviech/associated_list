9e0174ebd49f252007d4ec4cad626fad97e78e18
spamviech <spamviech@web.de>
2024-01-28 13:13:42 +0100

    implement tests for entry api

--- src/test/entry.rs
+++ src/test/entry.rs
@@ -1,5 +1,8 @@
 //! Unit tests for the [`Entry`]-API of an [`AssocList`].
 
+// signal failure in cases where `assert!` doesn't work, e.g. in a let-else.
+#![allow(clippy::panic)]
+
 use crate::{assoc_list, AssocList, Entry};
 
 #[test]
@@ -24,11 +27,129 @@ fn entry() {
 }
 
 #[test]
-fn occupied_entry() {
-    todo!()
+fn occupied_entry_key() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(occupied_entry) = assoc_list.entry("occupied") else {
+        panic!("Entry not occupied!");
+    };
+    assert_eq!(*occupied_entry.key(), OCCUPIED);
 }
 
 #[test]
-fn vacant_entry() {
-    todo!()
+fn occupied_entry_get() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(occupied_entry) = assoc_list.entry(OCCUPIED) else {
+        panic!("Entry not occupied!");
+    };
+    assert_eq!(*occupied_entry.get(), VALUE);
+}
+
+#[test]
+fn occupied_entry_get_mut() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    const NEW_VALUE: &str = "new value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(occupied_entry) = assoc_list.entry(OCCUPIED) else {
+        panic!("Entry not occupied!");
+    };
+    let value = occupied_entry.get_mut();
+    assert_eq!(*value, VALUE);
+    *value = NEW_VALUE;
+    assert_eq!(assoc_list.get(OCCUPIED), Some(&NEW_VALUE));
+    assert_eq!(assoc_list.get(ANOTHER), Some(&ANOTHER_VALUE));
+}
+
+#[test]
+fn occupied_entry_remove_entry() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(occupied_entry) = assoc_list.entry(OCCUPIED) else {
+        panic!("Entry not occupied!");
+    };
+    let (key, value) = occupied_entry.remove_entry();
+    assert_eq!(key, OCCUPIED);
+    assert_eq!(value, VALUE);
+    assert!(!assoc_list.contains_key(OCCUPIED));
+    assert!(assoc_list.contains_key(ANOTHER));
+}
+
+#[test]
+fn occupied_entry_remove() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(occupied_entry) = assoc_list.entry(OCCUPIED) else {
+        panic!("Entry not occupied!");
+    };
+    let value = occupied_entry.remove();
+    assert_eq!(value, VALUE);
+    assert!(!assoc_list.contains_key(OCCUPIED));
+    assert_eq!(assoc_list.get(ANOTHER), Some(&ANOTHER_VALUE));
+}
+
+#[test]
+fn occupied_entry_insert() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    const NEW_VALUE: &str = "new value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Occupied(mut occupied_entry) = assoc_list.entry(OCCUPIED) else {
+        panic!("Entry not occupied!");
+    };
+    let value = occupied_entry.insert(NEW_VALUE);
+    assert_eq!(value, VALUE);
+    assert_eq!(*occupied_entry.get(), NEW_VALUE);
+    assert_eq!(assoc_list.get(OCCUPIED), Some(&NEW_VALUE));
+    assert_eq!(assoc_list.get(ANOTHER), Some(&ANOTHER_VALUE));
+}
+
+#[test]
+fn vacant_entry_key() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    const VACANT: &str = "vacant";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Vacant(vacant_entry) = assoc_list.entry(VACANT) else {
+        panic!("Entry not vacant!");
+    };
+    assert_eq!(*vacant_entry.key(), VACANT);
+}
+
+#[test]
+fn vacant_entry_insert() {
+    const OCCUPIED: &str = "occupied";
+    const VALUE: &str = "value";
+    const ANOTHER: &str = "another";
+    const ANOTHER_VALUE: &str = "another value";
+    const VACANT: &str = "vacant";
+    const NEW_VALUE: &str = "new value";
+    let mut assoc_list = assoc_list!((OCCUPIED, VALUE), (ANOTHER, ANOTHER_VALUE));
+    let Entry::Vacant(vacant_entry) = assoc_list.entry(VACANT) else {
+        panic!("Entry not vacant!");
+    };
+    let new_value = vacant_entry.insert(NEW_VALUE);
+    assert_eq!(*new_value, NEW_VALUE);
+    assert_eq!(assoc_list.get(VACANT), Some(&NEW_VALUE));
+    assert_eq!(assoc_list.get(OCCUPIED), Some(&VALUE));
+    assert_eq!(assoc_list.get(ANOTHER), Some(&ANOTHER_VALUE));
 }
